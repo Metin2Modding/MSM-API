51bacad06fe84b0cb0deb4ec58ec759c145ce325
Thorek <75499293+Thorek777@users.noreply.github.com>
2022-08-20 23:23:49 +0200

    Update

--- Source/MSM-API/Tests/Main.cpp
+++ Source/MSM-API/Tests/Main.cpp
@@ -12,10 +12,10 @@ int main()
 	// One more thing - use '\t' instead of tabs, I did it just only for a better look.
 	vMSM.InsertLine("	Group ShapeData" + LAST_SHAPE_DATA_INCREASE);
 	vMSM.InsertLine("	{");
-	vMSM.InsertLine("		ShapeIndex 81759");
-	vMSM.InsertLine("		Model \"warrior_summer_2020.gr2\"");
-	vMSM.InsertLine("		SourceSkin \"warrior_summer_2020_1.dds\"");
-	vMSM.InsertLine("		TargetSkin \"warrior_summer_2020_2.dds\"");
+	vMSM.InsertLine("		ShapeIndex");
+	vMSM.InsertLine("		Model \"\"");
+	vMSM.InsertLine("		SourceSkin \"\"");
+	vMSM.InsertLine("		TargetSkin \"\"");
 	vMSM.InsertLine("	}");
 
 	// And the most important thing - save our changes to new file.
