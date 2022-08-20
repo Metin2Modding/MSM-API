e43aa7d0110e8f1eb22690167f9ebd78dd865c0a
Thorek <75499293+Thorek777@users.noreply.github.com>
2022-08-20 22:51:41 +0200

    Updated Main.cpp

--- MSM/Main.cpp
+++ MSM/Main.cpp
@@ -11,10 +11,10 @@ int main()
 	// If DataCount was increased, we can insert sample group to created earlier file.
 	vMSM.InsertLine("\tGroup ShapeData" + std::to_string(MSM::GetDataCount(vMSM.GetDataLine("Group ShapeData")) + 1));
 	vMSM.InsertLine("\t{");
-	vMSM.InsertLine("\t\tShapeIndex			81759");
-	vMSM.InsertLine("\t\tModel				\"warrior_summer_2020.GR2\"");
-	vMSM.InsertLine("\t\tSourceSkin			\"warrior_summer_2020_1.dds\"");
-	vMSM.InsertLine("\t\tTargetSkin			\"warrior_summer_2020_2.dds\"");
+	vMSM.InsertLine("\t\tShapeIndex 81759");
+	vMSM.InsertLine("\t\tModel \"warrior_summer_2020.GR2\"");
+	vMSM.InsertLine("\t\tSourceSkin \"warrior_summer_2020_1.dds\"");
+	vMSM.InsertLine("\t\tTargetSkin \"warrior_summer_2020_2.dds\"");
 	vMSM.InsertLine("\t}");
 
 	// And the most important thing - save our changes to new file.
